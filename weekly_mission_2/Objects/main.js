c9df32b4d88e2ba5af3cc3c4158e20c3af6dc85f
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-13 15:18:42 -0500

    Add object that recives parameters

--- weekly_mission_2/Objects/main.js
+++ weekly_mission_2/Objects/main.js
@@ -44,3 +44,21 @@ const helium = {
 console.log("This is an object with methods")
 console.log(helium)
 helium.sayHello()
+
+//Create object that recives parameters
+const copper = {
+    name: "Cuprum",
+    number: 29,
+    symbol: "Cu",
+    electricCurrent: function(volts){
+        if(volts > 12){
+            console.log("¡Puuum! ¡Electric Explotion!")
+        }else{
+            console.log("The light is on")
+        }
+    }
+}
+console.log("This is an object that recives parameters")
+console.log(copper)
+copper.electricCurrent(10)
+copper.electricCurrent(15)
