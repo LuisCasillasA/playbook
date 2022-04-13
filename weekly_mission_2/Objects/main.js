8d5996a8479e36129eee92d6b730dcc5face1826
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-13 14:57:39 -0500

    Add objects with differents properties

--- weekly_mission_2/Objects/main.js
+++ weekly_mission_2/Objects/main.js
@@ -11,3 +11,23 @@ const argentum = {
 }
 console.log("This is an object with properties")
 console.log(argentum);
+
+//Create Object with differents properties
+const gold = {
+    name: "Aurum",
+    number: 79,
+    symbol: "Au",
+    oxidationStates: [3, 1],
+    radio: {
+        atomic: 165,
+        covalent: 153,
+        vanWaals: 172
+    }
+}
+console.log("This is an object with differents properties")
+console.log(gold)
+console.log(`Gold Properties
+------------------------
+Gold name: ${gold.name}
+Gold symbol: ${gold.symbol}
+Gold atomic radio: ${gold.radio.atomic}`)
