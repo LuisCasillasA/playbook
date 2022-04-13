47acb9aab986a9bd815afce66741727cf077be04
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-13 14:40:20 -0500

    Create object whit properties

--- weekly_mission_2/Objects/main.js
+++ weekly_mission_2/Objects/main.js
@@ -2,3 +2,12 @@
 const stone = {}
 console.log("This is an empty object")
 console.log(stone)
+
+//Create object with properties
+const argentum = {
+    name: "Argentum",
+    number: 47,
+    symbol: "Ag"
+}
+console.log("This is an object with properties")
+console.log(argentum);
