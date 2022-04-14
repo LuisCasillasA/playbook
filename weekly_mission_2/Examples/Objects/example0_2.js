943f19268b0b52ea6ba120d198b390432a6c1128
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-14 12:53:33 -0500

    Add example object with properties

---
+++ weekly_mission_2/Examples/Objects/example0_2.js
@@ -0,0 +1,8 @@
+//Create object with properties
+const argentum = {
+    name: "Argentum",
+    number: 47,
+    symbol: "Ag"
+}
+console.log("This is an object with properties")
+console.log(argentum);
