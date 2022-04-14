8c5d1f9ef0a94718c24d5431d609206a66e35962
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-14 13:36:53 -0500

    Add example for each sum

---
+++ weekly_mission_2/Examples/Operators/example2.js
@@ -0,0 +1,7 @@
+// Example2: for each to calculate the sum of all elements in a list.
+let sum = 0;
+const numbers2 = [1, 2, 3, 4, 5];
+numbers2.forEach(num => sum += num)
+console.log("Example 2: Sum of all elements in a list.")
+console.log("The list is: " + numbers2)
+console.log("Sum : " + sum)
