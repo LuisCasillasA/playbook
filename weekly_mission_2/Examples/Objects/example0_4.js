a050617518a682d6aa035168f7e31aa2951c2cb1
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-14 12:58:10 -0500

    Add example objects with methods

---
+++ weekly_mission_2/Examples/Objects/example0_4.js
@@ -0,0 +1,12 @@
+//Create Object with methods
+const helium = {
+    name: "Helios",
+    number: 2,
+    symbol: "He",
+    sayHello: function(){
+        console.log('Hello World (Helium voice)')
+    }
+}
+console.log("This is an object with methods")
+console.log(helium)
+helium.sayHello()
