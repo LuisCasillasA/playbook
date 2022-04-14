e0b40c43213d5baf67e723c1d486c7116b345413
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-14 13:40:52 -0500

    Add example for each print elements in uppercase

---
+++ weekly_mission_2/Examples/Operators/example3.js
@@ -0,0 +1,4 @@
+// Example 3: For each to print elements in a list in uppercase
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+console.log("Example 5: Print elements in uppercase using for each.")
+countries.forEach((element) => console.log(element.toUpperCase()))
