3681fcc1219dcc043de39bffe63d4ce6665b5038
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-18 15:14:37 -0500

    Add example 5

---
+++ weekly_mission_2/Examples/Operators/example5.js
@@ -0,0 +1,6 @@
+// Example 5: using Map to convert all elements in a list to lowercase
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const namesToLowerCase = names.map((name) => name.toLowerCase())
+
+console.log("Example 5: using Map to convert all elements in a list to lowercase")
+console.log(namesToLowerCase)
