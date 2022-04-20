7a318b77767c5cacc17dd2483656033a52c36485
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-20 12:36:25 -0500

    Add user

---
+++ weekly_mission_2/exercises/exercise1/mainTwitter.js
@@ -0,0 +1,10 @@
+const user ={
+    name: 'Luis',
+    username: 'LuisCasillas',
+    age: 25,
+    profilePic: 'foto.png',
+    following: 10,
+    followers: 12,
+    bio: 'Mis gutos e intereses',
+    birthDate: new Date(1996, 12, 04)
+}
