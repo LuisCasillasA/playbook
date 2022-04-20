8d765bac5ac85486ece0462a958440ff48356da0
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-20 12:50:55 -0500

    Add method to user

--- weekly_mission_2/exercises/exercise1/mainTwitter.js
+++ weekly_mission_2/exercises/exercise1/mainTwitter.js
@@ -1,10 +1,16 @@
 const user ={
     name: 'Luis',
+    lastName: 'Casiilas',
     username: 'LuisCasillas',
     age: 25,
     profilePic: 'foto.png',
     following: 10,
     followers: 12,
     bio: 'Mis gutos e intereses',
-    birthDate: new Date(1996, 12, 04)
+    birthDate: new Date(1996, 12, 04),
+    getGeneralInfo: function(){
+        return `${this.name} ${this.lastName}   @${this.username} \nFollowing: ${this.following}\nFollowers: ${this.followers}\nBirth Date: ${this.birthDate}`
+    }
 }
+
+console.log(user.getGeneralInfo())
