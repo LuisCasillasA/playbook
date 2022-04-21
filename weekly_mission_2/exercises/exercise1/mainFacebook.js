2d46de7e492b7023e0d0445cc531acb5eb177ac2
LuisCasillasA <launchx10359@innovaccion.mx>
2022-04-20 20:47:01 -0500

    Add post

--- weekly_mission_2/exercises/exercise1/mainFacebook.js
+++ weekly_mission_2/exercises/exercise1/mainFacebook.js
@@ -8,7 +8,7 @@ const user ={
     birthDate: new Date(1996, 11, 04),
     work: 'LaunchX',
     from: 'Zacatecas',
-    maritalStatus: 'Solo solin solito',
+    maritalStatus: 'Solo solín solito',
     joinedDate: new Date(2013, 08, 03),
 
     getGeneralInfo: function(){
@@ -17,3 +17,62 @@ const user ={
 }
 
 console.log(user.getGeneralInfo())
+
+const post ={
+    user: user,
+    datePost: new Date(),
+    post: 'Este es un nuevo post',
+    tag: ['posteando'],
+    photo: 'photo.png',
+    reactions: {
+        like: 10,
+        love: 100,
+        care: 0,
+        fun: 10,
+        amaze:0,
+        sad: 0,
+        angry: 8
+    },
+    comments: [
+        {
+            user: user,
+            comment: 'Este es un comentario',
+            dateComment: new Date(),
+            reactions:{
+                like: 2,
+                love: 1
+            },
+            getReactions: function(){
+                return `Like: ${this.reactions.like} Love: ${this.reactions.love}`
+            }
+        },
+        {
+            user: user,
+            comment: 'Este es otro comentario',
+            dateComment: new Date(),
+            reactions:{
+                like: 5,
+                fun: 9
+            },
+            getReactions: function(){
+                return `Like: ${this.reactions.like} Fun: ${this.reactions.fun}`
+            }
+        }
+    ],
+    getPostInfo: function(){
+        return `${this.user.name} ${this.user.lastName}\n${this.datePost.toDateString("en-US")}\n${this.post}\n${this.photo}`
+    },
+    getReactions: function(){
+        return `Likes: ${this.reactions.like} love: ${this.reactions.love}`  
+    },
+    getComments: function(){
+        for (n in this.comments){
+            let comment = this.comments[n]
+            console.log(`\n${comment.user.name}\n${comment.comment}\n${comment.getReactions()}`)
+        }
+    }
+}
+console.log("\nPost")
+console.log(post.getPostInfo())
+console.log(post.getReactions())
+post.getComments()
