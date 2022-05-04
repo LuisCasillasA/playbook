4992fd54f857dc25d01594a9635c49d8c3df6604
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-03 19:07:48 -0500

    Driver and passanger

--- weekly_mission_2/exercises/exercise3/mainUber.js
+++ weekly_mission_2/exercises/exercise3/mainUber.js
@@ -1,9 +1,18 @@
 //Instance User Uber class
 import UserUber from './userUber.js'
 
-const userUber = new UserUber('Luis', 'Casillas', 25, 'photo.png', '1996, 12, 04', 4.8)
+const userPassanger = new UserUber('Luis', 'Casillas', 25, 'photo.png', '1996, 12, 04', 4.8)
 
 //Instance car class
 import Car from './car.js'
 
 const car = new Car('BMW M Sport', 'black', 'AZM9590')
+
+//Create User Driver
+const userDriver = new UserUber('Daniel', 'Ibarra', 28, 'photo.png', '1992, 01, 18', 4.5)
+userDriver.setCar(car)
+
+console.log('Passanger\n')
+console.log(userPassanger.getGeneralInfo())
+console.log('\nDriver')
+console.log(userDriver.getGeneralInfo() + userDriver.car.getCarInfo())
